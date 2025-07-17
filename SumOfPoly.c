110c3697c8b5e94391584d0020c9b71c7c7c6e31
mariaspatani <mariaspatani2028@cs.sjcetpalai.ac.in>
2025-07-17 12:11:12 +0530

    Update SumOfPoly.c

--- SumOfPoly.c
+++ SumOfPoly.c
@@ -5,68 +5,63 @@ Description : A program to find the sum of two polynomials.
 */
 
 #include <stdio.h>
-#define MAX 100
-
-struct Term {
-    int coeff;
-    int expo;
+struct polynomial{
+int coeff;
+int expo;
 };
-
-int main() {
-    struct Term poly1[MAX], poly2[MAX], resultPoly[MAX];
-    int i = 0, j = 0, k = 0;
-    int size1, size2;
-
-    printf("Enter the Size of Poly1:");
-    scanf("%d", &size1);
-    printf("Enter the elements in Poly1(coeff and expo)\n");
-    for (i = 0; i < size1; i++) {
-        printf("Term: %d ", i + 1);
-        scanf("%d %d", &poly1[i].coeff, &poly1[i].expo);
+int main(){
+  struct polynomial poly1[3];
+  struct polynomial poly2[3];
+  struct polynomial result[6];
+  int n1,n2,i=0,j=0,k=0;
+  printf("enter no of terms in polynomial1: ");
+  scanf("%d", &n1);
+  printf("enter terms in descending order  of exponent. \n");
+  for(int i=0;i<n1;i++){
+    printf("enter coefficient and exponent of term %d :",i+1);
+    scanf("%d%d",&poly1[i].coeff,&poly1[i].expo);
+  }
+  printf("enter no of terms in polynomial2: ");
+  scanf("%d", &n2);
+  printf("enter terms in descending order  of exponent: \n");
+  for(int j=0;j<n2;j++){
+    printf("enter coefficient and exponent of term %d ",j+1);
+    scanf("%d%d",&poly2[j].coeff,&poly2[j].expo);
+  }
+  while (i<n1 && j<n2){
+    if (poly1[i].expo==poly2[j].expo){
+      result[k].coeff=poly1[i].coeff+poly2[j].coeff;
+      result[k].expo=poly1[i].expo;
+      i++,j++,k++;
     }
+    else if (poly1[i].expo >poly2[j].expo){
+       result[k].coeff=poly1[i].coeff;
+       result[k].expo=poly1[i].expo;
+       i++,k++;
+   }
+   else{
+     result[k].coeff=poly2[j].coeff;
+     result[k].expo=poly2[j].expo;
+     j++,k++;
+   }
+ }
+ while (i<n1){
+  result[k]=poly1[i];
+  i++,k++;
+ }
+ while (j<n2){
+  result[k]=poly2[j];
+  j++,k++;
+ }
+ for (int i=0;i<k;i++){
+  printf("%d x^%d ",result[i].coeff, result[i].expo);
+  if (i != k - 1) {
+    printf("+ ");
+  }  
 
-    printf("Enter the Size of Poly2:");
-    scanf("%d", &size2);
-    printf("Enter the elements in Poly2(coeff and expo)\n");
-    for (j = 0; j < size2; j++) {
-        printf("Term: %d ", j + 1);
-        scanf("%d %d", &poly2[j].coeff, &poly2[j].expo);
-    }
-
-    i = j = 0;
-    while (i < size1 && j < size2) {
-        if (poly1[i].expo == poly2[j].expo) {
-            resultPoly[k].expo = poly1[i].expo;
-            resultPoly[k].coeff = poly1[i].coeff + poly2[j].coeff;
-            k++;
-            i++;
-            j++;
-        } else if (poly1[i].expo > poly2[j].expo) {
-            resultPoly[k] = poly1[i];
-            k++;
-            i++;
-        } else {
-            resultPoly[k] = poly2[j];
-            k++;
-            j++;
-        }
-    }
-
-    while (i < size1) {
-        resultPoly[k++] = poly1[i++];
-    }
-    while (j < size2) {
-        resultPoly[k++] = poly2[j++];
-    }
-
-    printf("Resultant Polynomial: ");
-    for (int m = 0; m < k; m++) {
-    if (resultPoly[m].coeff == 0)
-        continue;
-    printf(" + "); // ← This would print + even at the start
-    printf("%d", resultPoly[m].coeff);
-  
-}
+ }
+ return 0;
+}}
 
     return 0;
 }
